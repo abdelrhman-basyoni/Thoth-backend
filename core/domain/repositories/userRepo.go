29acb422687cda03ad2b5c5c94deb7fa3ee8353b
Abdelrhman Basyoni <64443028+abdelrhman-basyoni@users.noreply.github.com>
2023-11-16 23:36:42 +0200

    clear  cash

--- core/domain/repositories/userRepo.go
+++
@@ -1,8 +0,0 @@
-package domain
-
-import "github.com/abdelrhman-basyoni/thoth-backend/core/domain/entities"
-
-type UserRepository interface {
-	GetUserByEmail(email string) *entities.User
-	CreateUser(name, email, password, role string) error
-}
--- types/enums.go
+++
@@ -1,8 +0,0 @@
-package types
-
-type roles struct {
-	Admin  string
-	Author string
-}
-
-var Roles = &roles{Admin: "ADMIN", Author: "AUTHOR"}
